ea5c21adf19f5dba3b36a922cf4d465904cf1658
HONOR <89228157157@mail.ru>
2024-09-10 17:07:13 +0500

    Добавлена обработка исключений.

--- main.py
+++ main.py
@@ -1,18 +1,25 @@
 from tkinter import *       # Импортируем все классы и функции из модуля tkinter
 from tkinter import filedialog as fd      # Импортируем модуль для работы с диалогами файлов
 from tkinter import ttk     # Импортируем ttk, чтобы использовать более современные виджеты
+from tkinter import messagebox as mb     # Импортируем messagebox для отображения сообщений
 import requests     # Импортируем библиотеку requests для выполнения HTTP-запросов
 from ttkthemes import ThemedStyle  # Импортируем ThemedStyle из ttkthemes
 
 
 def upload():
-    filepath = fd.askopenfilename()     # Открываем диалоговое окно для выбора файла и сохраняем путь к выбранному файлу
-    if filepath:       # Проверяем, что файл был выбран
-        files = {"file": open(filepath, "rb")}      # Открываем файл для чтения в бинарном режиме и готовим его для отправки
-        response = requests.post("http://file.io", files=files)     # Отправляем файл на сервер с помощью POST-запроса
-        if response.status_code == 200:     # Если сервер успешно обработал запрос
-            link = response.json()["link"]      # Получаем ссылку на загруженный файл из ответа сервера
-            entry.insert(0, link)       # Вставляем полученную ссылку в текстовое поле (в начало строки)
+    try:
+        filepath = fd.askopenfilename()     # Открываем диалоговое окно для выбора файла и сохраняем путь к выбранному файлу
+        if filepath:       # Проверяем, что файл был выбран
+            with open(filepath, "rb") as f:
+                files = {"file": f}      # Открываем файл для чтения в бинарном режиме и готовим его для отправки
+                response = requests.post("http://file.io", files=files)     # Отправляем файл на сервер с помощью POST-запроса
+                response.raise_for_status()     # Проверяем, был ли запрос успешным
+                link = response.json()["link"]      # Получаем ссылку на загруженный файл из ответа сервера
+                entry.delete(0, END)        # Очищаем текстовое поле перед вставкой новой ссылки
+                entry.insert(0, link)       # Вставляем полученную ссылку в текстовое поле (в начало строки)
+    except Exception as e:      # Показываем сообщение об ошибке, если что-то пошло не так
+        mb.showerror("Ошибка", f"Произошла ошибка: {e}")
+
 
 window = Tk()       # Создаем основное окно приложения
 window.title("Сохранение файлов в облаке")
